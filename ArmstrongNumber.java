e20c8fc7a543d764eadf37615052175c3207db3d
Hema M <hemamurugesanece@gmail.com>
2017-07-07 15:26:11 +0530

    Update ArmstrongNumber.java

--- ArmstrongNumber.java
+++ ArmstrongNumber.java
@@ -4,14 +4,11 @@ class ArmstrongNumber
 {
 public static void main(String[] args)
 {
-Scanner sc=new Scanner(System.in);
-int m=sc.nextInt();
-int n=sc.nextInt();
-int a,p,sum=0,count=0;
-for(int i=m;i<n;i++)
+int a,p,sum=0;
+for(int i=1;i<250;i++)
 {
 p=i;
-while(i!=0)
+while(p>0)
 {
 a=p%10;
 sum=sum+(a*a*a);
